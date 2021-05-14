70fcba2343cbd8a39180b5f2d21916a53e06998b
tonl-g <tlygonneville.dev@gmail.com>
2021-05-14 15:20:02 +0200

    Add repo

---
+++ KingOfTheHill.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+// Pragma statements
+pragma solidity ^0.8.0;
+
+// Import statements
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+
+// Contract
+
+// Library usage
+
+// State variables
+
+// Events
+
+// constructor
+
+// Function modifiers
+
+// Functions
